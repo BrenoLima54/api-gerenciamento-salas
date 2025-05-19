e0f245bb5f12e65e2a4180111cfb7e3b4d7069b8
Breno Lima <116323977+BrenoLima54@users.noreply.github.com>
2025-05-19 20:26:02 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,27 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('../app');
 
 let isConnected = false;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Conectado ao MongoDB.");
-  isConnected = true;
-})
-.catch((err) => {
-  console.error("Erro ao conectar com o MongoDB:", err);
-});
+async function connectToDatabase() {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+
+    if (isConnected) {
+      console.log("Conectado ao MongoDB.");
+    }
+  } catch (err) {
+    console.error("Erro ao conectar com o MongoDB:", err);
+  }
+}
+
+connectToDatabase();
 
 module.exports = app;
